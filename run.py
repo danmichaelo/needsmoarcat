4fdd082fd67f7380538818c1d9c21569f5c8c417
Dan Michael O. Heggø <danmichaelo@gmail.com>
2019-08-04 21:13:11 +0200

    Rewrite/optimize

--- run.py
+++ run.py
@@ -1,42 +1,48 @@
 # encoding=utf-8
 # vim: sw=4 ts=4 expandtab ai
-import sys
+import itertools
 import os
-import pickle
 import pymysql.cursors
-from pymysql.cursors import Cursor
+from pymysql.cursors import SSCursor
 import re
 from datetime import datetime
 import mwclient
 import configparser
 import logging
+from tqdm import tqdm
 
 
-class ForceUnicodeCursor(Cursor):
+# ---------------------------------------------------------------------------------------
+# Bootstrapping
 
-    def _ensure_unicode_value(self, value):
+
+class ForceUnicodeCursor(SSCursor):
+
+    @staticmethod
+    def _ensure_unicode_value(value):
         if isinstance(value, bytes) or isinstance(value, bytearray):
             return value.decode('utf-8')
         return value
 
-    def _ensure_unicode(self, row):
+    def _conv_row(self, row):
+        if row is None:
+            return None
         return [self._ensure_unicode_value(value) for value in row]
-
-    def _do_get_result(self):
-        conn = self._get_db()
-
-        self._result = result = conn._result
-
-        self.rowcount = result.affected_rows
-        self.description = result.description
-        self.lastrowid = result.insert_id
-        self._rows = [self._ensure_unicode(row) for row in result.rows]
 
 
 class MyFormatter(logging.Formatter):
     def format(self, record):
         record.relativeSecs = record.relativeCreated / 1000
         return super(MyFormatter, self).format(record)
+
+
+def chunked_iterable(iterable, size):
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, size))
+        if not chunk:
+            break
+        yield chunk
 
 
 logger = logging.getLogger()
@@ -47,170 +53,147 @@ formatter = MyFormatter("%(relativeSecs).1fs [%(levelname)s] %(message)s")
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-
     
 def tprint(line):
     print(line, end='\r', flush=True)
 
+
 def fprint(line):
-    logger.info(line)
-
-db = pymysql.connect(
-    cursorclass=ForceUnicodeCursor,
-    host='nowiki.labsdb',
-    db='nowiki_p',
-    read_default_file=os.path.expanduser('~/replica.my.cnf')
-)
-
-db.ping(True)
+    logger.info(line + '                                      ')
 
 
 def get_config(fname):
-    config = {}
     parser = configparser.ConfigParser()
     parser.read([fname])
     return parser
 
 
-def get_subcats(cat_name, exceptions, hidden, path=[]):
-    cats = [cat_name]
+def dump_sorted(lines, filename):
+    with open(filename, 'w') as fp:
+        for line in sorted(list(lines)):
+            fp.write('%s\n' % line)
+
+
+# ---------------------------------------------------------------------------------------
+# Database query functions
+
+
+def get_hidden_categories(db, msg):
+    # Get a list of all hidden categories
+    tprint(msg)
     with db.cursor() as cursor:
         cursor.execute(
-            'SELECT page.page_title FROM page, categorylinks WHERE categorylinks.cl_to=%s '
-            'AND categorylinks.cl_type="subcat" AND categorylinks.cl_from=page.page_id',
-            [cat_name]
+            "SELECT page.page_title FROM page, page_props"
+            " WHERE page.page_id=page_props.pp_page AND page.page_namespace=14"
+            " AND page_props.pp_propname='hiddencat'"
         )
-        for row in cursor:
-            subcat = row[0]
-            # fprint(subcat)
-            if subcat in hidden and subcat not in exceptions:
-                if len(path) > 12:
-                    fprint("WARN: cat path exceeds max depth: {}".format(' > '.join(path)))
-                    # sys.exit(1)
-                else:
-                    subcat_members = get_subcats(subcat, exceptions, hidden, path + [cat_name])
-                    cats.extend(subcat_members)
-                    # fprint('%s: %d categories             ' % (subcat, len(subcat_members)), end='\r', flush=True)
-                    #if len(path) < 1:
-                    #    fprint('')
-    return list(set(cats))
+        return set([row[0] for row in cursor])
 
 
-def get_memberpages(msg, patterns, truncate=True):
-    # Returns a set of page IDs as ints, not namespace-filtered
+def get_category_memberships(db, msg):
+    # Get category memberships for all articles (pages with namespace 0)
+    # Returns dict on the format {page_id: int: [page_title: str, category_names: set)
+    out = {}
     tprint(msg)
-    if truncate:
-        qargs = ['{}%'.format(x) for x in patterns]
-        qstr = ' OR '.join(['cl_to LIKE %s' for x in patterns])
-        qstr = 'SELECT cl_from FROM categorylinks WHERE ({}) AND cl_type="page" GROUP BY cl_from'.format(qstr)
-    else:
-        qargs = ['{}'.format(x) for x in patterns]
-        qstr = 'cl_to IN ({})'.format( ('%s,' * len(patterns)).rstrip(','))
-        qstr = 'SELECT cl_from FROM categorylinks WHERE ({}) AND cl_type="page" GROUP BY cl_from'.format(qstr)
-    with db.cursor() as cursor:
-        # fprint(qstr)
-        # fprint(qargs)
-        cursor.execute(qstr, qargs)
-        pages = [row[0] for row in cursor]
-    return list(set(pages))
-
-
-def get_pagecats(msg, pageids):
-    chunksize = 10000
-    cats = {}
-    tprint(msg)
-    with db.cursor() as cursor:
-        for n in range(0, len(pageids), chunksize):
-            tp = pageids[n:n+chunksize]
-            cursor.execute('SELECT cl_from, cl_to FROM categorylinks WHERE cl_from IN ({})'.format(('%s,' * len(tp)).rstrip(',')), tp)
+    with tqdm(desc=msg) as pbar:
+        with db.cursor() as cursor:
+            cursor.execute(
+                "SELECT cl.cl_from, page.page_title, cl.cl_to "
+                " FROM categorylinks AS cl, page"
+                " WHERE cl.cl_type='page'"
+                " AND cl.cl_from=page.page_id"
+                " AND page.page_namespace=0"
+                # " LIMIT 10000"
+            )
             for row in cursor:
-                if row[0] not in cats:
-                    cats[row[0]] = []
-                cats[row[0]].append(row[1])
-            tprint(msg + ': %d of %d' % ( n + chunksize, len(pageids)))
-    return cats
+                if row[0] not in out:
+                    out[row[0]] = [row[1], set([row[2]])]
+                else:
+                    out[row[0]][1].add(row[2])
+                pbar.update()
+    return out
 
 
-def get_hidden_cats():
-    chunksize = 1000
-    cats = []
-    with db.cursor() as cursor:
-        cursor.execute('SELECT page.page_title FROM page, page_props WHERE page.page_id=page_props.pp_page AND page.page_namespace=14 AND page_props.pp_propname="hiddencat"')
-        for row in cursor:
-            cats.append(row[0])
-    return cats
+# ---------------------------------------------------------------------------------------
+# Filter functions
 
 
-def only_trivial_cats(cats, matcher, maintenance_cats):
+def any_matching(cats, matcher):
     for cat in cats:
-        if cat not in maintenance_cats and not matcher.match(cat):
+        if matcher.match(cat):
+            return True
+    return False
+
+
+def all_from_set_or_matching(cats, hidden_categories, matcher):
+    for cat in cats:
+        if cat not in hidden_categories and not matcher.match(cat):
             return False
     return True
 
-def main(maintenance_cats):
-    bio_cats = ['Personer_fra_', 'Fødsler_i_', 'Dødsfall_i_']
-    ignorepages = 'Dødsfall_i_'
 
-    pageids = get_memberpages('Getting list of biography pages', bio_cats)
-    fprint('Found %s biographies' % len(pageids))
+def all_from_set(page_categories, hidden_categories):
+    for cat in page_categories:
+        if cat not in hidden_categories:
+            return False
 
-    pagecats = get_pagecats('Getting categories for the biography pages', pageids)
-    fprint('Found cats for %s pages' % len(list(pagecats.keys())))
+    return True
 
-    tprint('Testing the biography pages against the list of maintenance categories')
-    r1 = re.compile(r'(' + '|'.join(bio_cats) + ')')
-    plist = []
-    for pageid, cats in list(pagecats.items()):
-        if only_trivial_cats(cats, r1, maintenance_cats):
-            plist.append(pageid)
-    fprint('Found %s biography pages lacking non-trivial categories' % len(plist))
+# ---------------------------------------------
 
-    tprint('Getting titles for the matched pages')
-    titles = []
-    with db.cursor() as cursor:
-        for page_id in plist:
-            cursor.execute('SELECT page_title FROM page WHERE page_id=%s AND page_namespace=0 LIMIT 1', [int(page_id)])
-            page = cursor.fetchone()
-            if page is not None:
-                page_title = page[0]
-                if re.match(ignorepages, page_title) is None:
-                    titles.append(page_title.replace('_', ' '))
-    fprint('Found %s matching titles' % len(titles))
+
+def check_page_categories(page_categories, hidden_categories):
+    msg = 'Checking the %d pages' % len(page_categories.keys())
+    tprint(msg)
+    out = []
+    for page_id in page_categories.keys():
+        if all_from_set(page_categories[page_id], hidden_categories):
+            out.append(page_id)
+    return out
+
+
+# ---------------------------------------------------------------------------------------
+# Job functions
+
+
+def kategorifattige_biografier(category_links, hidden_cats):
+    # Wikipedia:Kategorifattige biografier
+
+    cat_patterns = ['Personer_fra_', 'Fødsler_i_', 'Dødsfall_i_']
+    matcher = re.compile(r'(' + '|'.join(cat_patterns) + ')')
+
+    titles = set()
+    for page_id, page in category_links.items():
+        if any_matching(page[1], matcher) and all_from_set_or_matching(page[1], hidden_cats, matcher):
+            titles.add(page[0])
+
+    fprint('Wikipedia:Kategorifattige biografier: Found %d matching pages' % len(titles))
+    dump_sorted(titles, 'kategorifattige_biografier.txt')
 
     return titles
 
 
-def main2(maintenance_cats):
-    pageids = get_memberpages('Getting list of category members for the maintenance cats', maintenance_cats, False)
-    fprint('Found {} pages'.format(len(pageids)))
+def kun_vedlikeholdskategorier(category_links, hidden_cats):
+    # Wikipedia:Artikler med kun vedlikeholdskategorier
 
-    pagecats = get_pagecats('Getting categories for the pages', pageids)
-    fprint('Found cats for {} pages'.format(len(list(pagecats.keys()))))
+    titles = set()
+    for page_id, page in category_links.items():
+        if all_from_set(page[1], hidden_cats):
+            titles.add(page[0])
 
-    tprint('Testing %d pages' % len(pagecats.keys()))
-    plist = []
-    for pageid, cats in list(pagecats.items()):
-        others = [x for x in cats if x not in maintenance_cats]
-        if len(others) == 0:
-            plist.append(pageid)
+    fprint('Wikipedia:Artikler med kun vedlikeholdskategorier: Found %d matching pages' % len(titles))
+    dump_sorted(titles, 'kun_vedlikeholdskategorier.txt')
 
-    tprint('Checking {} matching pages'.format(len(plist)))
-    titles = []
-    with db.cursor() as cursor:
-        for page_id in plist:
-            cursor.execute('SELECT page_title FROM page WHERE page_id=%s AND page_namespace=0 LIMIT 1', [int(page_id)])
-            page = cursor.fetchone()
-            if page is not None:
-                page_title = page[0]
-                titles.append(page_title.replace('_', ' '))
-
-    fprint('Found {} matching titles'.format(len(titles)))
     return titles
 
+
+# ---------------------------------------------------------------------------------------
+# Formatting and page updating
 
 def update_page(site, pagename, titles):
     if titles is None or len(titles) == 0:
         return
+    titles = [title.replace('_', ' ') for title in titles]
     page = site.pages[pagename]
     origtext = page.text()
     beginlistmarker = '<!--BegynnListe-->'
@@ -221,37 +204,111 @@ def update_page(site, pagename, titles):
     text = '\n'.join('* [[{}]]'.format(title) for title in titles)
     dt = datetime.now().strftime('%Y-%m-%d')
     newtext = '{}{}{:d} sider (oppdatert {}):\n\n{}'.format(origtext[0:p], beginlistmarker, len(titles), dt, text)
-    fprint('New text ready')
     # fprint(newtext)
     page.save(newtext, 'Bot: Oppdaterer liste')
     fprint('New text saved')
 
+# ---------------------------------------------------------------------------------------
+# Main
 
-conf = get_config('config.cnf')
-mwsite = dict(conf.items('site'))
-mwoptions = dict(conf.items('options'))
-site = mwclient.Site(**mwsite)
 
-maintenance_exceptions = [
-    'Mangler_interwiki',
-    'Kategorier_som_trenger_diffusjon',
-    'Artikler_som_bør_flettes',
-    'Artikler_som_bør_flyttes',
-    'Sider_som_er_foreslått_slettet',
-    'Kategorier_som_trenger_diffusjon',
-    'Kvinner',
-    'Menn',
-]
+def main():
 
-tprint('Getting list of hidden cats')
-hidden_cats = get_hidden_cats()
-fprint('Found %d hidden cats' % len(hidden_cats))
+    conf = get_config('config.cnf')
+    db_conf = dict(conf.items('db'))
+    if 'port' in db_conf:
+        db_conf['port'] = int(db_conf['port'])
 
-tprint('Getting list of maintenance categories')
-maintenance_cats = get_subcats('Wikipedia-vedlikehold', maintenance_exceptions, hidden_cats)
-fprint('Found %s maintenance categories' % len(maintenance_cats))
+    db = pymysql.connect(cursorclass=ForceUnicodeCursor, **db_conf)
+    db.ping(True)
 
-update_page(site, 'Wikipedia:Kategorifattige biografier', main(maintenance_cats))
-update_page(site, 'Wikipedia:Artikler med kun vedlikeholdskategorier', main2(maintenance_cats))
+    site_conf = dict(conf.items('site'))
+    site = mwclient.Site(**site_conf)
 
-fprint('Exiting')
+    hidden_cats = get_hidden_categories(db, 'Getting list of hidden categories')
+    dump_sorted(hidden_cats, 'hidden_cats.txt')
+    fprint('Found %d hidden cats' % len(hidden_cats))
+
+    category_links = get_category_memberships(db, 'Reading category memberships')
+    fprint('Read category memberships for %d pages' % len(category_links))
+
+    update_page(
+        site,
+        'Wikipedia:Kategorifattige biografier',
+        kategorifattige_biografier(category_links, hidden_cats)
+    )
+
+    update_page(
+        site,
+        'Wikipedia:Artikler med kun vedlikeholdskategorier',
+        kun_vedlikeholdskategorier(category_links, hidden_cats)
+    )
+
+    fprint('Exiting')
+
+
+if __name__ == '__main__':
+    main()
+
+
+# ---------------------------------------------------------------------------------------
+# Not in use anymore
+
+# def get_subcats(cat_name, exceptions, hidden, path=[]):
+#     cats = [cat_name]
+#     with db.cursor() as cursor:
+#         cursor.execute(
+#             "SELECT page.page_title FROM page, categorylinks"
+#             " WHERE categorylinks.cl_to=%s"
+#             " AND categorylinks.cl_type='subcat'"
+#             " AND categorylinks.cl_from=page.page_id",
+#             [cat_name]
+#         )
+#         for row in cursor:
+#             subcat = row[0]
+#             # fprint(subcat)
+#             if subcat in hidden and subcat not in exceptions:
+#                 if len(path) > 12:
+#                     fprint("WARN: cat path exceeds max depth: {}".format(' > '.join(path)))
+#                 else:
+#                     subcat_members = get_subcats(subcat, exceptions, hidden, path + [cat_name])
+#                     cats.extend(subcat_members)
+#     return list(set(cats))
+#
+#
+# def get_page_categories(msg, page_ids):
+#     chunksize = 10000
+#     out = {}
+#     tprint(msg)
+#     cur = 0
+#     tot = len(page_ids)
+#     with db.cursor() as cursor:
+#         for chunk in chunked_iterable(page_ids, chunksize):
+#             cursor.execute(
+#                 "SELECT cl_from, cl_to FROM categorylinks"
+#                 " WHERE cl_from IN (%s)" % ('%s,' * len(chunk)).rstrip(','),
+#                 chunk
+#             )
+#             for row in cursor:
+#                 cur += 1
+#                 if row[0] not in out:
+#                     out[row[0]] = set()
+#                 out[row[0]].add(row[1])
+#             tprint(msg + ': %d of %d' % (cur, tot))
+#     return out
+
+
+# def get_page_titles(page_ids):
+#     # Given a set of page IDs, fetch the corresponding page titles
+#     chunksize = 100
+#     tprint('Fetching page titles for %d pages' % len(page_ids))
+#     titles = set()
+#     with db.cursor() as cursor:
+#         for chunk in chunked_iterable(page_ids, chunksize):
+#             cursor.execute(
+#                 'SELECT page_title FROM page WHERE page_id IN (%s)' % ('%s,' * len(chunk)).rstrip(','),
+#                 chunk
+#             )
+#             titles.update([row[0] for row in cursor])
+#
+#     return titles
